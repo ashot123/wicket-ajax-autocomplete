4cb63ddbd2102a2aeef0b3286678e89a2254502f
Ashot K <ashot2005@yahoo.com>
2014-09-07 23:53:50 +0400

    Initial commit

---
+++ src/main/java/wicket/examples/ajax/WicketApplication.java
@@ -0,0 +1,21 @@
+package wicket.examples.ajax;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Created by Ashot Karakhanyan on 18-07-2014
+ */
+public class WicketApplication extends WebApplication {
+    @Override
+    public Class<? extends Page> getHomePage() {
+        //Application application = Application.get();
+        return AutoCompletePage.class;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        // add your configuration here
+    }
+}
